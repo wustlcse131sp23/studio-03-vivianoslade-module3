321607eee7288d37f865a7a683e154a08e9000c2
Angelina7355 <122848734+Angelina7355@users.noreply.github.com>
2024-09-20 23:27:28 -0500

    TA Update

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -12,26 +12,45 @@ public class Sieve {
 		System.out.print("Enter Limit: ");
 		int n = in.nextInt();
 		
-		boolean[] allNumbers = new boolean[n];
+		boolean[] allPrimeNumbers = new boolean[n];
 				
 		for (int i = 2; i < n; i++)
 		{
-			allNumbers[i] = true;
+			allPrimeNumbers[i] = true;
 			//System.out.println(allNumbers[i]);
 		}
+		
+		/**
 		for (int i = 2; i < Math.sqrt(n); i++)
 		{
-			if (allNumbers[i] == true)
+			if (allPrimeNumbers[i] == true)
 			{
 					for (int j = i*i; j < n; j+=i)
 					{
-						allNumbers[j] = false;
+						allPrimeNumbers[j] = false;
 					}
 			}
 		}
+		**/
+		// start at 2 bc 2 is the smallest prime number
+		for (int i = 2; i < n; i++) // external loop keeps track of which value we are finding multiples of
+		{
+			if (allPrimeNumbers[i] == true)
+			{
+				// start at 2i because i itself can be prime (i.e. 2 is prime but 4, 6, 8, etc. is not)
+				for (int j = 2*i; j < n; j += i)	// external loop keeps track of the multiples of i and marking non-prime indices as false;
+				{
+					allPrimeNumbers[j] = false;
+				}
+			}
+		}
+		
+		
+		
+		
 		for (int i = 2; i < n; i++)
 		{
-			if (allNumbers[i] == true)
+			if (allPrimeNumbers[i] == true)
 			{
 				System.out.println(i);
 			}
